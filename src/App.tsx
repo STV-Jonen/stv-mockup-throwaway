ab2a83297cd52b641eafe452040e1a258e0cc1fd
Levin Gsell <levin.gsell@bluewin.ch>
2024-12-06 14:59:02 +0100

    fixed linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { ForgotPasswordForm } from "./components/auth/ForgotPasswordForm";
 import { LoginForm } from "./components/auth/LoginForm";
 import { ResetPasswordForm } from "./components/auth/ResetPasswordForm";
 
-type Props = {};
-
-export const App = ({}: Props) => {
+export const App = () => {
   return (
     <div className="grid min-h-dvh grid-cols-3 items-start bg-gradient-to-bl from-primary to-primary/80 p-8">
       <LoginForm />
